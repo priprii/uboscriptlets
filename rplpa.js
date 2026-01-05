afed06408f240e52855f0feca2e67cf13001bc87
priprii <41253973+priprii@users.noreply.github.com>
2026-01-05 18:25:54 +0000

    Update rplpa.js

--- rplpa.js
+++ rplpa.js
@@ -1,8 +1,8 @@
 /// rplpa.js
-/// alias rplpa
+/// alias rplpa.js
 /// world ISOLATED
 /// dependency run-at.fn
-// example.com##+js(rplpa, selector, attr, search, replace)
+// example.com##+js(rplpa, [selector], attr, search, replace)
 function rplpa(
     selector = '',
     attr = '',
@@ -10,37 +10,44 @@ function rplpa(
     replace = ''
 ) {
     if ( selector === '' || attr === '' || search === '' ) { return; }
-
-    const apply = () => {
+    const replacepartialattr = ( ) => {
         const elems = document.querySelectorAll(selector);
-        for ( const el of elems ) {
-            if ( !el.hasAttribute(attr) ) { continue; }
-
-            const val = el.getAttribute(attr);
-            if ( typeof val !== 'string' ) { continue; }
-            if ( val.includes(search) === false ) { continue; }
-
-            el.setAttribute(attr, val.replaceAll(search, replace));
-        }
+        try {
+            for ( const el of elems ) {
+                if ( !el.hasAttribute(attr) ) { continue; }
+                const val = el.getAttribute(attr);
+                if ( typeof val !== 'string' ) { continue; }
+                if ( val.includes(search) === false ) { continue; }
+                el.setAttribute(attr, val.replaceAll(search, replace));
+            }
+        } catch { }
     };
-
-    let scheduled = false;
-    const observer = new MutationObserver(() => {
-        if ( scheduled ) { return; }
-        scheduled = true;
-        requestAnimationFrame(() => {
-            scheduled = false;
-            apply();
+    let observer, timer;
+    const onDomChanged = mutations => {
+        if ( timer !== undefined ) { return; }
+        let shouldWork = false;
+        for ( const mutation of mutations ) {
+            if ( mutation.addedNodes.length === 0 ) { continue; }
+            for ( const node of mutation.addedNodes ) {
+                if ( node.nodeType !== 1 ) { continue; }
+                shouldWork = true;
+                break;
+            }
+            if ( shouldWork ) { break; }
+        }
+        if ( shouldWork === false ) { return; }
+        timer = self.requestAnimationFrame(( ) => {
+            timer = undefined;
+            replacepartialattr();
         });
-    });
-
-    runAt(() => {
-        apply();
-        observer.observe(document.documentElement, {
-            childList: true,
+    };
+    const start = ( ) => {
+        if ( replacepartialattr() === false ) { return; }
+        observer = new MutationObserver(onDomChanged);
+        observer.observe(document.body, {
             subtree: true,
-            attributes: true,
-            attributeFilter: [ attr ],
+            childList: true,
         });
-    }, 'interactive');
+    };
+    runAt(( ) => { start(); }, 'interactive');
 }
